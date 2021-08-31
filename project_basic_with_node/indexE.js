de4aa0347bb63e0804cd0103f2f74ddd44e9faba
Lucas de Matos <lucasmatos592@gmail.com>
2021-08-31 19:03:30 -0300

    Using http and Express

---
+++ project_basic_with_node/indexE.js
@@ -0,0 +1,25 @@
+// Usando Express
+
+const express = require('express');
+const app = express();
+const path = require('path')
+const router = express.Router();
+
+
+
+router.get('/', function(req, res) {
+
+    res.sendFile(path.join(__dirname+'/index.html')); // localhost:3000
+
+})
+
+router.get('/sobre', function(req, res) {
+
+    res.sendFile(path.join(__dirname+'/sobre.html'));
+
+})
+
+app.use('/', router);
+app.listen(process.env.port || 3000); // localhost:3000/sobre
+
+console.log('Servidor rodando!');
